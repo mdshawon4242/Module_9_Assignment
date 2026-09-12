ca8ffedc97500831fb408c5138ce89d4df314ae7
Shawon Akondo <shawonakondo20@gmail.com>
2026-09-12 19:59:40 +0600

    Add Blog, About  & Contact Route

--- script.js
+++ script.js
@@ -11,7 +11,38 @@ const server = http.createServer((req, res) => {
       <a href="/contact">Contact</a>
     </nav>`);
     res.end();
-  } else {
+  }else if (req.url == "/blog") {
+    res.write(` <h1>Blog Page</h1>
+    <p>Here you will find our latest blog posts.</p>
+    <nav>
+      <a href="/">Home</a> |
+      <a href="/about">About</a> |
+      <a href="/blog">Blog</a> |
+      <a href="/contact">Contact</a>
+    </nav>`);
+    res.end()
+  }else if (req.url == "/contact") {
+    res.write(`<h1>Contact Page</h1>
+    <p>You can reach us at contact@example.com</p>
+    <nav>
+      <a href="/">Home</a> |
+      <a href="/about">About</a> |
+      <a href="/blog">Blog</a> |
+      <a href="/contact">Contact</a>
+    </nav>`);
+    res.end();
+  }else if (req.url == "/about") {
+    res.write(`<h1>About Page</h1>
+    <p>This is a simple Node.js HTTP server project.</p>
+    <nav>
+      <a href="/">Home</a> |
+      <a href="/about">About</a> |
+      <a href="/blog">Blog</a> |
+      <a href="/contact">Contact</a>
+    </nav>`);
+    res.end()
+  }
+  else {
     res.write(`<h1>404 - Page Not Found</h1>`);
     res.end();
   }
