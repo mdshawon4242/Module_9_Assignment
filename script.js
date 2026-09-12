d0790325dff5206795d29bb5ab4962a1752eb085
Shawon Akondo <shawonakondo20@gmail.com>
2026-09-12 19:52:24 +0600

    Add Home Route

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const http = require("http");
-
+const http = require('http');
 
 const server = http.createServer((req, res) => {
-  
-})
+  if (req.url == '/') {
+    res.write(`<h1>Home Page</h1>
+    <p>Welcome to our simple Node.js server!</p>
+    <nav>
+      <a href="/">Home</a> |
+      <a href="/about">About</a> |
+      <a href="/blog">Blog</a> |
+      <a href="/contact">Contact</a>
+    </nav>`);
+    res.end();
+  } else {
+    res.write(`<h1>404 - Page Not Found</h1>`);
+    res.end();
+  }
+});
+
 
 server.listen(5000, () => {
-  console.log("Server is running on port 5000")
-})
+  console.log('Server is running on port 5000');
+});
