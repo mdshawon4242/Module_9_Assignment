7a5d9c9c36bf0589f20aa86b4bdec07a2f981d35
Shawon Akondo <shawonakondo20@gmail.com>
2026-09-12 19:32:16 +0600

    Create HTTP Server for module 9 assignment

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("My name is Shawon")
+const http = require("http");
+
+
+const server = http.createServer((req, res) => {
+  
+})
+
+server.listen(5000, () => {
+  console.log("Server is running on port 5000")
+})
